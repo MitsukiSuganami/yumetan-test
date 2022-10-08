a118dfa78c39b5bf453c4cf8e4218ed8b839f272
Seto42357 <Seto42357@gmail.com>
2022-10-09 00:22:00 +0900

    css

--- testingCodes/question_e.php
+++ testingCodes/question_e.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Yumetan Words Test (Question)</title>
-        <link rel="stylesheet" href="css/common.css">
+        <link rel="stylesheet" href="css/common_pc.css">
     </head>
 
     <body>

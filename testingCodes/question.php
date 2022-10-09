4dbec3ebddb6813c96633bf13694dce70470aec3
Seto42357 <Seto42357@gmail.com>
2022-10-09 15:31:29 +0900

    ハイフンをアンダーバーに変更

--- testingCodes/question.php
+++ testingCodes/question.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>ユメタン単語テスト（問題）</title>
-        <link rel="stylesheet" href="css/common.css">
+        <link rel="stylesheet" href="css/common_pc.css">
     </head>
 
     <body>
